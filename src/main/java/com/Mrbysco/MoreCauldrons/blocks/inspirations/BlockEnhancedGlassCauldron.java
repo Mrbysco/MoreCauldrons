f43a1353eda72bcc6facb666b88774e8004524b0
Mrbysco <Heeybysco@live.nl>
2018-06-26 21:36:38 +0200

    -

--- src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/BlockEnhancedGlassCauldron.java
+++ src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/BlockEnhancedGlassCauldron.java
@@ -10,9 +10,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -65,11 +63,5 @@ public class BlockEnhancedGlassCauldron extends BlockEnhancedCauldronBase implem
     @Override
     public boolean canRenderInLayer(IBlockState state, BlockRenderLayer layer) {
     	return layer == BlockRenderLayer.TRANSLUCENT;
-    }
-    
-    @Override
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos,
-    		EnumFacing side) {
-    	return true;
     }
 }
