c85403f3c9e7326a828c8edbc4ee1d118cf3134d
Mrbysco <Mrbysco@users.noreply.github.com>
2021-01-12 13:47:56 +0100

    -

--- src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/EnhancedCauldronBase.java
+++ src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/EnhancedCauldronBase.java
@@ -1,10 +1,7 @@
 package com.mrbysco.morecauldrons.blocks.inspirations;
 
-import knightminer.inspirations.recipes.block.EnhancedCauldronBlock;
-import net.minecraft.block.Block;
-
-public class EnhancedCauldronBase extends EnhancedCauldronBlock {
-	public EnhancedCauldronBase(Block.Properties properties){
-		super(properties);
-	}
+public class EnhancedCauldronBase {//extends EnhancedCauldronBlock {
+//	public EnhancedCauldronBase(Block.Properties properties){
+//		super(properties);
+//	}
 }
--- src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/EnhancedWoodenCauldronBlock.java
+++ src/main/java/com/Mrbysco/MoreCauldrons/blocks/inspirations/EnhancedWoodenCauldronBlock.java
@@ -1,28 +1,19 @@
 package com.mrbysco.morecauldrons.blocks.inspirations;
 
-import knightminer.inspirations.recipes.block.EnhancedCauldronBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SoundType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
-import net.minecraftforge.common.ToolType;
-
 @SuppressWarnings("deprecated")
-public class EnhancedWoodenCauldronBlock extends EnhancedCauldronBlock implements ICauldron{
+public class EnhancedWoodenCauldronBlock {//extends EnhancedCauldronBlock implements ICauldron{
 
-	public EnhancedWoodenCauldronBlock(Block.Properties properties){
-		super(properties.sound(SoundType.WOOD).harvestTool(ToolType.AXE).hardnessAndResistance(2.0F, 3.0F));
-	}
+//	public EnhancedWoodenCauldronBlock(Block.Properties properties){
+//		super(properties.sound(SoundType.WOOD).harvestTool(ToolType.AXE).hardnessAndResistance(2.0F, 3.0F));
+//	}
 
-	@Override
-	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-		return 10;
-	}
+//	@Override
+//	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+//		return 10;
+//	}
 
-	@Override
-	public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-		return 5;
-	}
+//	@Override
+//	public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+//		return 5;
+//	}
 }
