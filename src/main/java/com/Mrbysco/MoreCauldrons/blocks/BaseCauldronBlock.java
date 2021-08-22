620fab5b782cdc0f665325f97dda15b58457d6bd
Mrbysco <Mrbysco@users.noreply.github.com>
2021-08-22 20:46:06 +0200

    Another attempt at making custom fluid containers work on Cauldrons

--- src/main/java/com/Mrbysco/MoreCauldrons/blocks/BaseCauldronBlock.java
+++ src/main/java/com/Mrbysco/MoreCauldrons/blocks/BaseCauldronBlock.java
@@ -19,9 +19,59 @@ import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 public class BaseCauldronBlock extends CauldronBlock {
 	
 	public BaseCauldronBlock(Block.Properties properties){
 		super(properties);
+	}
+
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+		ItemStack itemstack = player.getHeldItem(handIn);
+		if (itemstack.isEmpty()) {
+			return ActionResultType.PASS;
+		} else {
+			int i = state.get(LEVEL);
+			if (itemstack.getMaxStackSize() == 1 && FluidUtil.getFluidHandler(itemstack).isPresent()) {
+				IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(itemstack).orElse(null);
+				Optional<FluidStack> containedFluid = FluidUtil.getFluidContained(itemstack);
+				Supplier<FluidStack> waterStackSupplier = () -> new FluidStack(Fluids.WATER, 1000);
+				if (containedFluid.isPresent() && i < 3 && !worldIn.isRemote) {
+					//Container has liquid
+					FluidStack fluidStack = containedFluid.get();
+					if (fluidStack.getFluid() == Fluids.WATER && fluidStack.getAmount() >= 1000) {
+						FluidStack drainedStack = fluidHandler.drain(waterStackSupplier.get(), FluidAction.SIMULATE);
+						if (drainedStack.getAmount() == 1000) {
+							fluidHandler.drain(waterStackSupplier.get(), FluidAction.EXECUTE);
+							player.setHeldItem(handIn, fluidHandler.getContainer());
+							player.addStat(Stats.FILL_CAULDRON);
+							this.setWaterLevel(worldIn, pos, state, 3);
+							worldIn.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+						}
+					}
+				} else {
+					//Container is empty
+					if (i == 3 && !worldIn.isRemote) {
+						if (!player.abilities.isCreativeMode) {
+							if (fluidHandler.isFluidValid(0, waterStackSupplier.get()) && fluidHandler.fill(waterStackSupplier.get(), FluidAction.SIMULATE) == 1000) {
+								fluidHandler.fill(waterStackSupplier.get(), FluidAction.EXECUTE);
+								player.setHeldItem(handIn, fluidHandler.getContainer());
+							}
+						}
+
+						player.addStat(Stats.USE_CAULDRON);
+						this.setWaterLevel(worldIn, pos, state, 0);
+						worldIn.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+					}
+
+				}
+				return ActionResultType.SUCCESS;
+			}
+		}
+
+		return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
 	}
 }
